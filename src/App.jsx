4bf82a9053fb642fb66a9a3b4673a2da65d7dc84
Rai_Froh <raifroh@gmail.com>
2023-07-31 18:17:10 -0700

    Experiences setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data.jsx"
 import './App.css'
 
 function App() {
-
+  const experiencesData = data.map(experience => {
+    return <Card img={experience.coverImg} rating={experience.rating} reviewCount={experience.stats.rating} location={experience.location} title={experience.title} price={experience.price} />
+})
   return (
   <div>
     <Navbar />
-    <Hero />
+   {/* <Hero /> */}
+   {experiencesData}
   </div>
   )
 }
